fbe35a4cffba368e7340bf9b70cda8568570d262
Hamza Ahmad <hamzahmd.dev@gmail.com>
2021-08-27 17:04:59 +0500

    changing links background

--- src/components/Projects/ProjectsStyles.js
+++ src/components/Projects/ProjectsStyles.js
@@ -84,15 +84,15 @@ export const UtilityList = styled.ul`
 `;
 
 export const ExternalLinks = styled.a`
-  color: #d4c0c0;
+  color: #fff;
   font-size: 1.6rem;
   padding: 1rem 1.5rem;
 
-  background: #6b3030;
+  background: linear-gradient(270deg, #1b4e59 0%, #6bbfa0 100%);
   border-radius: 15px;
   transition: 0.5s;
   &:hover {
-    background: #801414;
+    background: linear-gradient(270deg, #1e3a49 0%, #60a692 100%);
   }
 `;
 
--- src/components/TimeLine/TimeLine.js
+++ src/components/TimeLine/TimeLine.js
@@ -15,13 +15,8 @@ const Timeline = () => {
         building efficient projects both for clients and to enhance my skillset.
         I started my web development journey after pandemic in 2020 by realizing
         the potential & growth opportunities. Later, while gradually enhancing
-        my skills, I got my passion in this field.
-      </SectionText>
-      <SectionText>
-        Web app development is something that I always wanted to do. I'm a
-        Mechatronics Engineering graduate. When I was building my final year
-        project with my team, the only thing lacking in my project was the
-        application to complete the product.
+        my skills, I got my passion in this field. Web app development is
+        something that I always wanted to do.
       </SectionText>
       <SectionText>
         I'm a kind of person who doesn't stop unless explore something.
